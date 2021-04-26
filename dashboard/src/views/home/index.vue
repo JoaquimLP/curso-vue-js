d5829e958782e4500dc5cef0672f2cb63684903e
JoaquimLP <joaquimlp2013@gmail.com>
2021-04-26 19:43:51 -0300

    Fix

---
+++ dashboard/src/views/home/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <custom-header
+        @create-account="handleAccountCreate"
+        @login="handleLogin"
+    />
+    <contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">Feedbacker &copy; 2021</p>
+    </div>
+
+</template>
+
+<script>
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+export default {
+    components : { CustomHeader, Contact},
+    setup()
+    {
+        function handleLogin()
+        {
+
+        }
+
+        function handleAccountCreate()
+        {
+
+        }
+
+        return
+        {
+            handleLogin,
+            handleAccountCreate
+        }
+    }
+
+}
+</script>
